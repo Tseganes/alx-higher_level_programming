6b9f787fdb411d5397c81cc5a42a8fe2cb1f5b62
Tseganes <tsegakebede20@gmail.com>
2023-10-09 03:48:17 -0700

    inher

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module base_geometry
+Creates a Class
+"""
+
+class BaseGeometry:
+    def area(self):
+        """Raises an exception with the message
+        'area() is  not implemented'
+        """
+
+        raise Exception('area() is not implemented')
