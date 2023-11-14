d306a30b5488702eb953289b64ab7ff74dbbbe7b
Tseganes <tsegakebede20@gmail.com>
2023-11-14 03:17:16 -0800

    sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- lists all rows of the table second_table
+-- from the database hbtn_0c_0
+-- results display score and name
+-- ordered by score
+-- DESC command is used to sort the data returned in descending order.
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- lists all rows of the table first_table
+-- from the database htbn_0c_0
+SELECT * FROM first_table;
