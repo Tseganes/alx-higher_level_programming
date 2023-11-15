ecf5638707acd7feeb86acdadbddca286a800e7f
Tseganes <tsegakebede20@gmail.com>
2023-11-15 03:21:19 -0800

    sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 -- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) 
 -- id INT unique, auto generated, can’t be null and is a primary key
--- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa'
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'cities'
+-- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
        (id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
-        state id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
-	name VARCHAR(256) NOT NULL);
+       state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+       name VARCHAR(256) NOT NULL);
